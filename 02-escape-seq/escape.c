d2e8da0bab4fc3aa65d3ea3dc0b1ba760e2902e0
demonkiller <gaurangvishwakarma619@gmail.com>
2025-01-21 17:16:15 +0000

    02 Escape Squence

---
+++ 02-escape-seq/escape.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// This is a single line comment in C
+int main()
+{
+    printf("Hello World \n"); // \n is an escape sequence for new line
+    printf("Let's test the tab \t escape sequence \n");
+    return 0; 
+}
+
+/*
+This is a multiline comment
+We can add multiple lines of comments like this.
+*/

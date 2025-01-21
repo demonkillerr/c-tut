987ab8a3dd7d94973e9771821a10e34ed03e3049
demonkiller <gaurangvishwakarma619@gmail.com>
2025-01-21 17:23:27 +0000

    03 variables

---
+++ 03-variable/var.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+// This is a single line comment in C
+int main()
+{
+    int sum;
+
+    sum = 10 + 20;
+
+    printf("Sum of 10 and 20 is: %d \n", sum);
+
+    return 0;
+}
+
+/*
+int x;          //declaration
+x = 123;        //initialization
+int y = 321;    //declation and initialization
+
+Types of Variables:
+int x = 10;             // integer
+float y = 11.5;         // floating point 
+char z = 's';           // character
+char name[] = "dmkl";   //array of characters (emulation of strings)
+*/

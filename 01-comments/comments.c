e975b2238d574a3bf25707f4fb95fb418c780412
demonkiller <gaurangvishwakarma619@gmail.com>
2025-01-21 17:13:30 +0000

    add comments section

---
+++ 01-comments/comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// This is a single line comment in C
+int main()
+{
+    printf("Hello World \n");
+    return 0; // comments can be added like this as well
+}
+
+/*
+This is a multiline comment
+We can add multiple lines of comments like this.
+*/
